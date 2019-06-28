0bdea0007f9c51385fc43256678c109bd373d28e
alpaca623 <alpaca623@gmail.com>
2019-06-28 16:21:30 +0900

    [updating] : add style source

--- src/Components/HomeComponent/WeeklyNotice.js
+++ src/Components/HomeComponent/WeeklyNotice.js
@@ -2,17 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { readDocumentApi } from "../../api";
+import { routes } from "../../routes";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding:10px;
   width: 700px;
   height: 500px;
   border: 1px solid black;
   margin: 20px;
 `;
 
+const DocumentListBtn = styled.div`
+  color: blue;
+  margin: 20px 0;
+`;
+
 const DocumentUl = styled.ul``;
 
-const DocumentLi = styled.li``;
+const DocumentLi = styled.li`
+  margin: 10px 0;
+`;
 
 const DocumentTitle = styled.p``;
 
@@ -21,6 +32,11 @@ class WeeklyNotice extends React.Component {
     const { documents } = this.props;
     return (
       <Container>
+        <DocumentListBtn>
+          <Link to={`${routes.document}${routes.documentList}`}>
+            <h3>공지사항 전체 보기</h3>
+          </Link>
+        </DocumentListBtn>
         <DocumentUl>
           {documents ? (
             documents.map(document => (
